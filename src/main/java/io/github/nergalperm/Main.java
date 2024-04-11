4f63da34f50e168946394949b4d53264a7dd415c
Евгений Терехов <nergal@psu.ru>
2024-04-11 19:47:41 +0400

    feat: ability to run the specific problem

--- src/main/java/io/github/nergalperm/Main.java
+++ src/main/java/io/github/nergalperm/Main.java
@@ -1,7 +1,28 @@
 package io.github.nergalperm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    private static Map<String, Entrypoint> entrypoints = new HashMap<>();
+
+    static {
+       // Is empty for now
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length==0) {
+            System.out.println("Need to pass the ID of the problem");
+            return;
+        }
+        String problemId = args[0];
+        entrypoints.getOrDefault(
+            problemId,
+            () -> System.out.printf("There is no problem with id: %s%n", problemId)
+        ).run();
+    }
+
+    public interface Entrypoint {
+        void run();
     }
 }
