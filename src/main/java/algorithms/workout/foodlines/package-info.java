a8cef6e69e735bbfc88ddbcc75acc9f7c4534b84
Евгений Терехов <nergal@psu.ru>
2024-06-11 18:22:50 +0400

    docs: added problem description to the solution package

---
+++ src/main/java/algorithms/workout/foodlines/package-info.java
@@ -0,0 +1,36 @@
+/**
+ * The problem ID is: <a href="https://dmoj.ca/problem/lkp18c2p1">lkp18c2p1</a>
+ * <p>
+ * <br/><br/>
+ *
+ * <strong>Problem Description</strong>
+ * <p>
+ * <br/><br/>
+ * <p>
+ * After many years of continuous warfare, the country of Collea was left in shambles. This has
+ * caused food shortages and famines all across the country. In an attempt to distribute the little
+ * food that was produced and prevent food hoarding, the Collean government limited the amount of
+ * food that any one person can buy. Soon, Collean citizens are forced to wait in long lines in
+ * order to obtain the food they needed. There are currently <em>N</em> such food lines in the city
+ * of Lachtin, the <em>i</em>-th of which has <em>a</em><sub>i</sub> people in it. <em>M</em> people
+ * are going to enter one of the lines in the next hour, where they each enter the shortest line
+ * they see. Since Phreia plans to enter the line, she wants to know the length of the line that
+ * each person decides to join.
+ * <p>
+ * <br/><br/>
+ *
+ * <strong>Input Specification</strong>
+ * <p>
+ * <br/><br/>
+ * <p>
+ * The first line contains two positive integers, N and M.<br/> The second line contains N positive
+ * integers, a<sub>1</sub>, a<sub>2</sub>, … , a<sub>N</sub> .
+ * <p>
+ * <br/><br/>
+ *
+ * <strong>Output Specification</strong>
+ * <p>
+ * <br/><br/> Print M lines, the <em>i</em>-th of which being the length of the line that the
+ * <em>i</em>-th person joined.
+ */
+package algorithms.workout.foodlines;
