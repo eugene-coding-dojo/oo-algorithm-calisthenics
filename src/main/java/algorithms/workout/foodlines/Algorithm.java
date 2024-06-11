e70fd494c5521e1e67d6e3bfc802642b795b704d
Евгений Терехов <nergal@psu.ru>
2024-06-11 19:00:09 +0400

    refactor: inlined shortestLineIndex method

--- src/main/java/algorithms/workout/foodlines/Algorithm.java
+++ src/main/java/algorithms/workout/foodlines/Algorithm.java
@@ -4,20 +4,16 @@ public class Algorithm {
     public static int[] solve(int n, int m, int[] lines) {
         int[] lineJoinHistory = new int[m];
         for (int i = 0; i < m; i++) {
-            int shortestLineIndex = shortestLineIndex(lines);
+            int shortestLineIndex = 0;
+            for (int i1 = 1; i1 < lines.length; i1++) {
+                if (lines[i1] < lines[shortestLineIndex]) {
+                    shortestLineIndex = i1;
+                }
+            }
             lineJoinHistory[i] = lines[shortestLineIndex];
             lines[shortestLineIndex]++;
         }
         return lineJoinHistory;
     }
 
-    private static int shortestLineIndex(int[] lines) {
-        int shortestLineIndex = 0;
-        for (int i = 1; i < lines.length; i++) {
-            if (lines[i] < lines[shortestLineIndex]) {
-                shortestLineIndex = i;
-            }
-        }
-        return shortestLineIndex;
-    }
 }
