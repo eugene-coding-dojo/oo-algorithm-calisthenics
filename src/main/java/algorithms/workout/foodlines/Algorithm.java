d624af511a731308dbbe75c433ca39c9b916be5b
Евгений Терехов <nergal@psu.ru>
2024-06-11 19:11:00 +0400

    refactor: extracted two method finding the shortest line index

--- src/main/java/algorithms/workout/foodlines/Algorithm.java
+++ src/main/java/algorithms/workout/foodlines/Algorithm.java
@@ -5,15 +5,25 @@ public class Algorithm {
         int[] lineJoinHistory = new int[m];
         for (int i = 0; i < m; i++) {
             int shortestLineIndex = 0;
-            for (int i1 = 1; i1 < lines.length; i1++) {
-                if (lines[i1] < lines[shortestLineIndex]) {
-                    shortestLineIndex = i1;
-                }
-            }
+            shortestLineIndex = shortestLineIndex(lines, shortestLineIndex);
             lineJoinHistory[i] = lines[shortestLineIndex];
             lines[shortestLineIndex]++;
         }
         return lineJoinHistory;
+    }
+
+    private static int shortestLineIndex(int[] lines, int shortestLineIndex) {
+        for (int j = 1; j < lines.length; j++) {
+            shortestLineIndex = shortestAmongTwoLines(lines, j, shortestLineIndex);
+        }
+        return shortestLineIndex;
+    }
+
+    private static int shortestAmongTwoLines(int[] lines, int j, int shortestLineIndex) {
+        if (lines[j] < lines[shortestLineIndex]) {
+            shortestLineIndex = j;
+        }
+        return shortestLineIndex;
     }
 
 }
