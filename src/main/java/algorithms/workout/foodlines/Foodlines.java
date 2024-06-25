a86e50d9a66783777437247b9b100fce70509058
Евгений Терехов <nergal@psu.ru>
2024-06-25 18:48:01 +0400

    refactor: find the shortest line using stream and compareTo

--- src/main/java/algorithms/workout/foodlines/Foodlines.java
+++ src/main/java/algorithms/workout/foodlines/Foodlines.java
@@ -10,22 +10,7 @@ public class Foodlines {
         this.foodlines = Arrays.stream(lines).mapToObj(Foodline::new).toList();
     }
 
-    public int indexOfTheShortest() {
-        int shortestLineIndex = 0;
-        for (int j = 1; j < this.foodlines.size(); j++) {
-            shortestLineIndex = shortestAmongTwoLines(j, shortestLineIndex);
-        }
-        return shortestLineIndex;
-    }
-
-    private int shortestAmongTwoLines(int j, int shortestLineIndex) {
-        if (this.foodlines.get(j).compareTo(this.foodlines.get(shortestLineIndex)) <= 0) {
-            shortestLineIndex = j;
-        }
-        return shortestLineIndex;
-    }
-
     public Foodline shortestLine() {
-        return this.foodlines.get(this.indexOfTheShortest());
+        return this.foodlines.stream().min(Foodline::compareTo).orElseThrow();
     }
 }
